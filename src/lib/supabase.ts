3867e01901334a5d7bb2f4bd10b68ed6d5722c34
Eureka <caesarioarya288@gmail.com>
2024-01-22 14:38:40 +0700

    integration with prisma

---
+++ src/lib/supabase.ts
@@ -0,0 +1,77 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseClient = createClient(process.env.NEXT_API_PUBLIC_SUPABASE!!, process.env.NEXT_API_PUBLIC_SUPABASE_KEY!!)
+
+const createId = (length: number) => {
+	let result = "";
+	const characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const charactersLength = characters.length;
+	let counter = 0;
+	while (counter < length) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength)
+		);
+		counter += 1;
+	}
+	return result;
+};
+
+export const supabaseUploadFile = async(file:File | string, bucket: 'company' | 'applicant' ) =>{
+    const filename = `${createId(6)}.jpg`;
+
+	const { data, error } = await supabaseClient.storage
+		.from(bucket)
+		.upload("public/" + filename, file, {
+			cacheControl: "3600",
+			upsert: false,
+		});
+
+	return {
+		data,
+		error,
+		filename,
+	};
+}
+
+export const supabaseGetPublicUrl = (filename: string, bucket: 'company' | 'applicant') =>{   
+    
+    const { data } = supabaseClient.storage
+    .from(bucket)
+    .getPublicUrl("public/" + filename);
+
+    return {
+        publicUrl: data.publicUrl,
+    };
+
+}
+export const supabaseUpdateFile = async(file: File | string,filename: string,bucket: "company" | "applicant") =>{
+
+    const { data, error } = await supabaseClient
+    .storage
+    .from(bucket)
+    .update("public/" + filename, file, {
+        cacheControl: '3600',
+        upsert: true
+    })
+
+    return {
+        data,
+        error,
+    }
+}
+export const supabaseDeleteFile = async(file: File | string,filename: string,bucket: "company" | "applicant") =>{
+    
+    const { data, error } = await supabaseClient.storage
+		.from(bucket)
+		.update("public/" + filename, file, {
+			cacheControl: "3600",
+			upsert: true,
+		});
+
+	return {
+		data,
+		error,
+	};
+}
